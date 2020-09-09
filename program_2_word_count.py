65c53fe95ede43255b5801cbf89b13cb8ee00e14
ROKINAXTREME <70982347+ROKINAXTREME@users.noreply.github.com>
2020-09-08 20:53:51 -0500

    Add files via upload

---
+++ program_2_word_count.py
@@ -0,0 +1,12 @@
+print('this program count the charecters the you inputed to the computer')
+var_1=input('GIMME SOME INPUT!')
+print(len(var_1))
+print(var_1.upper())
+#import math
+print(abs(-4/3))
+print(round(abs(-4/3)))
+print(min(34,234,345,56,23))
+a = [348, 67, 1]
+print(sorted(a))
+print('a'+str(sum(a)))
+
